6061682208fdf776ce04e358e0cf0e5f427592d6
Danny Lee <dreamer@panix.com>
2020-01-24 10:01:18 -0500

    first codeout

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 // Code your solution here!
+let myString = 'pizza';
 
+function printString(myString) {
+  console.log(myString[0])
+
+  if (myString.length > 1) {
+    let mySubString = myString.substring(1, myString.length)
+    printString(mySubString) 
+  } else {
+    return true
+  }
+}
+
+printString(myString)
