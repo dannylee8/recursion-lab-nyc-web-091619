10a5d81f63b3d9e4a056429df7c90626b7d5ca8a
Danny Lee <dreamer@panix.com>
2020-01-24 10:45:31 -0500

    add addUpTo and palindrom functions

--- index.js
+++ index.js
@@ -12,4 +12,28 @@ function printString(myString) {
   }
 }
 
-printString(myString)
+function reverseString (myString) {
+  console.log(myString[0])
+  if (myString.length < 2) {
+    return myString
+  } else {
+    return reverseString(myString.substring(1)) + myString[0]
+  }
+}
+
+function isPalindrome (myString) {
+  const l = myString.length
+
+  if (l < 2) {
+    return true
+  } else if (myString[l- 1] === myString[0]) {
+    return isPalindrome(myString.substring(1, l - 1))
+  } else {
+    return false
+  }
+}
+
+function addUpTo (myArray, index) {
+  return index ? myArray[index] + addUpTo(myArray, --index) : myArray[index]
+}
+
